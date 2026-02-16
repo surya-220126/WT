21fc057f20cbdbd289f98397002101a6d3ae5341
surya-220126 <n220126@rguktn.ac.in>
2026-02-16 17:01:01 +0530

    oauth lab updated

--- OAUTH/login.php
+++ OAUTH/login.php
@@ -2,12 +2,12 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId("278127621826-11urhseoaku0ruv5smd23phpv327mg9p.apps.googleusercontent.com");
-$client->setClientSecret("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");
+$client->setClientId("client id");
+$client->setClientSecret("client secret");
 $client->setRedirectUri("http://localhost/WT-XAMPP/OAUTH/callback.php");
 $client->addScope("email");
 $client->addScope("profile");
 
 $authUrl = $client->createAuthUrl();
 header("Location: " . $authUrl);
-exit();
+exit()
